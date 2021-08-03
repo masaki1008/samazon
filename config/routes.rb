2f682caa674ba61fcbc38e61273125973762ef82
侍 太郎 <kobayashimasaki1008@gamil.com>
2021-08-03 07:28:54 +0000

    16 管理画面の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,21 @@
 Rails.application.routes.draw do
+
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+
+  devise_scope :admin do
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    get "dashboard/logout", :to => "admins/sessions#destroy"
+  end
+
+  namespace :dashboard do
+    resources :categories, except: [:new]
+    resources :products, except: [:show]
+  end
+
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
