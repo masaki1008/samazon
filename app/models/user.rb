9db4e135db07191185e2fcae491ca982f727f6c5
侍 太郎 <kobayashimasaki1008@gamil.com>
2021-08-04 16:52:24 +0000

    16 ユーザー管理画面の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,19 @@
 class User < ApplicationRecord
   has_many :reviews
+  extend DisplayList
+  extend SwitchFlg
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable
   acts_as_liker
 
-  def update_password(params, *options)
-    if params[:password].blank?
-      params.delete(:password)
-      params.delete(:password_confirmation) if params[:password_confirmation].blank?
-    end
-
-    result = update(params, *options)
-    clean_up_passwords
-    result
-  end
+  scope :search_information, -> (keyword) {
+    where("name like ?","%#{keyword}%").
+    or(where("email like ?","%#{keyword}%")).
+    or(where("address like ?","%#{keyword}%")).
+    or(where("postal_code like ?","%#{keyword}%")).
+    or(where("phone like ?","%#{keyword}%")).
+    or(where("id ?","%#{keyword}%"))
+  }
 end
